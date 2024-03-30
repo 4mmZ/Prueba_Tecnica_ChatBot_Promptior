e1f7e0ac98d9b999e5a6bff3bd02bad9ebd9b2a2
4mmZ <113656077+4mmZ@users.noreply.github.com>
2024-03-30 00:16:57 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -18,4 +18,4 @@ add_routes(app, chain, path="/chat_bot")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
