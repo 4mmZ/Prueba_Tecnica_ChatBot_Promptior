e25b8587daf965d7338d29dbcc539afd974e245b
4mmZ <113656077+4mmZ@users.noreply.github.com>
2024-03-30 01:07:48 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     
-    uvicorn.run("app.server:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+    uvicorn.run("app.server:app", --host 0.0.0.0 --port $PORT)
